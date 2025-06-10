c0075bf65b7fd0c87e3ed96d02ec406979816a02
mistercow <esteban.lagos21@gmail.com>
2025-06-10 16:13:31 -0400

    Creación del proyecto de API de libros

---
+++ src/main/java/com/estebanlagos/bookcollector/BookcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.estebanlagos.bookcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estebanlagos/bookcollector/BookcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estebanlagos.bookcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

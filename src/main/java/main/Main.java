e87f2e790424f78f8ed1c51130af7d9ada006563
mistercow <esteban.lagos21@gmail.com>
2025-06-12 02:11:51 -0400

    Clase BookInfo funcionando, se puede obtener el top de libros mas descargados

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,18 @@
 package main;
 
+import model.BookInfo;
+import model.DataBook;
 import model.DataTotal;
 import service.BookApiClient;
 import service.DataConverter;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static final String URL_BASE = "https://gutendex.com/books/";
+    // paginas totales https://gutendex.com/books/?page=2378
+    private static final String URL_BASE = "https://gutendex.com/books/?page=1000";
     private BookApiClient bookApiClient = new BookApiClient();
     private DataConverter dataConverter = new DataConverter();
 
@@ -15,14 +20,37 @@ public class Main {
         var json = bookApiClient.fetchBooks(URL_BASE);
         System.out.println(json);
         var data = dataConverter.dataConverter(json, DataTotal.class);
+
+        /*
         data.booksList().stream()
-                .limit(5)
+                .limit(10)
                 .forEach(book -> {
-                    System.out.println(book.title() + "  ");
+                    System.out.println(book.title());
                     book.autorList().forEach(author -> {
                         System.out.println(author.authorName());
                     });
                 });
+        */
 
+        // Transforma los datos de los record DataBook y DataAuthor en solo una clase llamada BookInfo
+        List<BookInfo> books = data.booksList().stream()
+                .flatMap(dataBook -> dataBook.autorList().stream()
+                        .map(dataAuthor -> new BookInfo(
+                                dataBook.id(),
+                                dataBook.title(),
+                                dataAuthor.authorName(),
+                                dataAuthor.birthYear(),
+                                dataBook.numberDownload(),
+                                dataBook.languages()
+                        ))).collect(Collectors.toList());
+
+
+        System.out.println("Top 15 de libros descargados");
+        books.stream()
+                .sorted(Comparator.comparing(BookInfo::getNumbreDownload).reversed())
+                .limit(15)
+                .forEach(bookInfo -> System.out.println(
+                        "Título: " + bookInfo.getAuthorName() +
+                                " Total descargas: " + bookInfo.getNumbreDownload()));
     }
 }
--- src/main/java/model/BookInfo.java
+++ src/main/java/model/BookInfo.java
@@ -12,4 +12,72 @@ public class BookInfo {
     private List<String> languages;
     private Double numbreDownload;
 
+    public BookInfo(Integer id, String title, String authorName, Integer birthYear, Double numbreDownload, List<String> languages) {
+        this.id = id;
+        this.title = title;
+        this.authorName = authorName;
+        this.birthYear = birthYear;
+        this.numbreDownload = numbreDownload;
+        this.languages = languages;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
+    public Double getNumbreDownload() {
+        return numbreDownload;
+    }
+
+    public void setNumbreDownload(Double numbreDownload) {
+        this.numbreDownload = numbreDownload;
+    }
+
+    @Override
+    public String toString() {
+        return "BookInfo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", authorName='" + authorName + '\'' +
+                ", birthYear=" + birthYear +
+                ", languages=" + languages +
+                ", numbreDownload=" + numbreDownload +
+                '}';
+    }
 }
